b25b987dbd1a1caf9c2f011fc8bc990b28367312
Alexey Kuchinski <lamakaha@gmail.com>
2014-06-23 18:13:03 -0700

    Initial release

---
+++ cardToTrack/CardtoTrack.py
@@ -0,0 +1,155 @@
+def corn3D():
+    import nuke
+    first = nuke.Root().knob('first_frame').getValue()
+    first = int(first)
+    first = str(first)
+    last = nuke.Root().knob('last_frame').getValue()
+    last = int(last)
+    last = str(last)
+    basicRange = first+"-"+last
+    ret = nuke.getFramesAndViews('get range, Your camera, card and bg have to be selected!!!', basicRange)
+    range = ret[0]
+    print 'range is', range
+    rangeA = range.split("-")[0]
+    rangeA=int(rangeA)
+    rangeB = range.split("-")[1]
+    rangeB=int(rangeB)    
+    n = nuke.selectedNodes("Card2")
+    for n in n:
+        x = n['xpos'].value()
+        y = n['ypos'].value()
+
+        trans = n['translate'].value()
+        rot = n['rotate'].value()
+        scalex = n['scaling'].value(0)
+        scaley = n['scaling'].value(1)
+        
+        mainA = nuke.nodes.Axis()
+        mainA['translate'].setValue(trans)
+        mainA['rotate'].setValue(rot)
+        mainA['name'].setValue("mainA")
+    
+        
+        LU = nuke.nodes.Axis()
+        LU['translate'].setValue([-0.5,0.5,0])
+        LU.setInput(0,mainA)
+        LU['name'].setValue('LU')
+        
+        RU = nuke.nodes.Axis()
+        RU['translate'].setValue([0.5,0.5,0])
+        RU.setInput(0,mainA)
+        RU['name'].setValue('RU')
+        
+        LL = nuke.nodes.Axis()
+        LL['translate'].setValue([-0.5,-0.5,0])
+        LL.setInput(0,mainA)
+        LL['name'].setValue('LL')
+        
+        RL= nuke.nodes.Axis()
+        RL['translate'].setValue([0.5,-0.5,0])
+        RL.setInput(0,mainA)
+        RL['name'].setValue('RL')
+    
+    n = nuke.selectedNodes()
+    for n in n:
+        if n.Class() != "Card2" and n.Class() != "Camera2":
+            BG=n
+        if n.Class() != "Card" and n.Class() != "Camera":
+            BG=n
+        if n.Class() == "Camera2":
+            Cam=n
+        if n.Class() == "Camera":
+            Cam=n
+    
+    LUP = nuke.nodes.Reconcile3D()
+    LUP.setInput(2,LU)
+    LUP.setInput(1,Cam)
+    LUP.setInput(0,BG)
+    LUP['name'].setValue("P4")
+    
+    RUP = nuke.nodes.Reconcile3D()
+    RUP.setInput(2,RU)
+    RUP.setInput(1,Cam)
+    RUP.setInput(0,BG)
+    RUP['name'].setValue("P3")
+    
+    
+    LLP = nuke.nodes.Reconcile3D()
+    LLP.setInput(2,LL)
+    LLP.setInput(1,Cam)
+    LLP.setInput(0,BG)
+    LLP['name'].setValue("P1")
+    
+    
+    RLP = nuke.nodes.Reconcile3D()
+    RLP.setInput(2,RL)
+    RLP.setInput(1,Cam)
+    RLP.setInput(0,BG)
+    RLP['name'].setValue("P2")
+
+
+
+    n = nuke.nodes.Tracker3()
+    n['enable1'].setValue(1)
+    n['enable2'].setValue(1)
+    n['enable3'].setValue(1)
+    n['enable4'].setValue(1)
+    
+    P1 = nuke.toNode("P1")
+    nuke.execute(P1,rangeA,rangeB)
+    P1p = P1['output'].value()
+    
+    P2 = nuke.toNode("P2")
+    nuke.execute(P2,rangeA,rangeB)
+    P2p = P2['output'].value()
+    
+    P3 = nuke.toNode("P3")
+    nuke.execute(P3,rangeA,rangeB)
+    P3p = P3['output'].value()
+    
+    P4 = nuke.toNode("P4")
+    nuke.execute(P4,rangeA,rangeB)
+    P4p = P4['output'].value()
+    
+    n['track1'].copyAnimations(P1['output'].animations())
+    n['track2'].copyAnimations(P2['output'].animations())
+    n['track3'].copyAnimations(P3['output'].animations())
+    n['track4'].copyAnimations(P4['output'].animations())
+    n['use_for1'].setValue(7)
+    n['use_for2'].setValue(7)
+    n['use_for3'].setValue(7)
+    n['use_for4'].setValue(7)
+
+    n['xpos'].setValue(x+100)
+    n['ypos'].setValue(y)
+    
+    
+    mainA = nuke.toNode("mainA")
+    LU = nuke.toNode("LU")
+    RU = nuke.toNode("RU")
+    LL = nuke.toNode("LL")
+    RL = nuke.toNode("RL")
+    
+    nuke.delete(mainA)
+    nuke.delete(LU)
+    nuke.delete(RU)
+    nuke.delete(LL)
+    nuke.delete(RL)
+    nuke.delete(P1)
+    nuke.delete(P2)
+    nuke.delete(P3)
+    nuke.delete(P4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
